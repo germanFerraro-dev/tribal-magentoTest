06743c91b9ba47a236346a0145119c6cdab25594
lonelyBits <64561247+lonelyBits@users.noreply.github.com>
2021-01-27 02:49:52 -0300

    Add files via upload

---
+++ app/code/Tribal/CustomModal/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ * @category    Tribal
+ * @package     Tribal_CustomModal
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Tribal_CustomModal',
+    __DIR__
+);
---
+++ app/design/frontend/Tribal/tribal-theme/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ *
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+  ComponentRegistrar::THEME,
+  'frontend/Tribal/tribal-theme',
+  __DIR__
+);
